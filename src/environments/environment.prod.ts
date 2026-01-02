bb9728f3d6f77f01e4df467b779772caf9c24a5c
joscanoav <joscanoav@gmail.com>
2026-01-02 15:56:55 +0100

    updating enviroments files

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  geminiApiKey: 'AIzaSyCmNXG_sI0jZRBv_YwfRd2vXMoOHV2tuPo'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  geminiApiKey: 'AIzaSyCmNXG_sI0jZRBv_YwfRd2vXMoOHV2tuPo'
+};
